e3b6d932207e5bde1012e26dbba527f42d5ca2b4
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-01-12 17:50:39 -0500

    added bare bones fx application

--- ArcGISparksApplicationUI/src/applications/MyParksMain.java
+++ ArcGISparksApplicationUI/src/applications/MyParksMain.java
@@ -1,14 +1,25 @@
 package applications;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MyParksMain extends Application{
+	
+	public static void main(String...args){launch(args);}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage stage) throws Exception {
 		
+		Group group = new Group();
+		Scene scene = new Scene(group);
+		
+		stage.setWidth(200);
+		stage.setHeight(200);
+		stage.setTitle("Test FX Application");
+		stage.setScene(scene);
+		stage.show();
 	}
 
 }
