c68980a72c5651436e565b08714b493afec0cf2f
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-01-12 21:26:55 -0500

    wired controller

--- ArcGISparksApplicationUI/src/controllers/ParkController.java
+++ ArcGISparksApplicationUI/src/controllers/ParkController.java
@@ -2,19 +2,29 @@ package controllers;
 
 import java.util.ArrayList;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-//import models.AccountModel;
+import javafx.scene.control.Button;
 
 public class ParkController {
 	
 	public static ArrayList<Node> myViewList = new ArrayList<Node>();
-	//public static AccountModel accountModel = new AccountModel();
 	
-	@FXML private Node applicationView;//view 0
+	@FXML Button button1;
+	@FXML Button button2;
+	
+	@FXML
+	private void button1Action(ActionEvent event){
+		System.out.println("Button1 pressed");
+	}
+	
+	@FXML
+	private void button2Action(ActionEvent event){
+		System.out.println("Button2 pressed");
+	}
 	
 	@FXML void initialize(){
-		//myViewList.get(0).setVisible(true);
 	}
 
 }
