fe7abe488978c0ba89ace83c882a918fc1ab0ac3
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-01-19 21:10:10 -0500

    added support class QueryState

---
+++ ArcGISparksApplicationUI/src/supportclasses/QueryState.java
@@ -0,0 +1,19 @@
+package supportclasses;
+
+public class QueryState {
+	
+	private boolean visited;
+	
+	public QueryState() {
+		this.visited = false;
+	}
+
+	public boolean getVisited() {
+		return visited;
+	}
+
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+
+}
