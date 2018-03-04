58161ea893d865a84ee96e41125c801515f3d1f4
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-03-04 14:50:23 -0500

    added camp app pane

---
+++ ArcGISparksApplicationUI/src/supportclasses/CampAppPane.java
@@ -0,0 +1,52 @@
+package supportclasses;
+
+import java.io.IOException;
+
+import entities.Site;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+
+public class CampAppPane {
+	
+	private FlowPane fpane;
+	private Site site;
+	
+	public CampAppPane() {
+		
+	}
+
+	public FlowPane getFpane() {
+		return fpane;
+	}
+
+	public void setFpane(FlowPane fpane) throws IOException {
+		this.fpane = fpane;
+		fpane.getChildren().add(createCampAppPane());
+	}
+
+	public Site getSite() {
+		return site;
+	}
+
+	public void setSite(Site site) {
+		this.site = site;
+	}
+	
+	public FlowPane createCampAppPane() throws IOException {
+		FlowPane root = new FlowPane();
+		
+		Pane view1 = FXMLLoader.load(getClass().getResource("/view1.fxml"));
+	    view1.setVisible(true);
+		
+		root.getChildren().add(view1);
+		
+		
+		return root;
+	}
+
+	
+	
+
+}
