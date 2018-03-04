3ab9b9bff69528d77d7417d1e5b462ce6129350d
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-03-04 15:00:06 -0500

    added webcrawler

---
+++ ArcGISparksApplicationUI/src/supportclasses/WebCrawler.java
@@ -0,0 +1,20 @@
+package supportclasses;
+
+import java.util.ArrayList;
+
+public class WebCrawler {
+	
+	public static ArrayList<String> crawl() {
+		ArrayList<String> myList = new ArrayList<String>();
+		myList.add("item 1 a");
+		myList.add("item 2 a");
+		myList.add("item 3 a");
+		myList.add("item 4 a");
+		
+		return myList;
+		
+		
+	}
+	
+
+}
