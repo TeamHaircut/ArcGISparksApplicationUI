22225478e9deb936517023e1ded32ec8ed17f71e
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-03-06 18:17:32 -0500

    updated camp app pane

--- ArcGISparksApplicationUI/src/models/CampAppModel.java
+++ ArcGISparksApplicationUI/src/models/CampAppModel.java
@@ -11,31 +11,25 @@ import javafx.collections.ObservableList;
 
 public class CampAppModel {
 
-	//list view property (items property)														list view property (items property)
-			ObjectProperty<ObservableList<String>> listviewProp = new SimpleObjectProperty<ObservableList<String>>();
-			public ObjectProperty<ObservableList<String>> listviewProperty() throws IOException {
-				listviewProp.setValue(FXCollections.observableList(getList()));
-				return listviewProp;
-			}
-			
-			public void setListviewProp(ObservableList<String> newListviewProp){
-				listviewProp.setValue(newListviewProp);	
-			}
-			
-			public ObservableList<String> getListviewProp(){
-				return listviewProp.getValue();
-			}
-			
-			
-	//list view (getList())																list view (getList())
-			public List<String> myList = new ArrayList<String>();
-			public List<String> getList() throws IOException{
-				myList = WebCrawler.crawl();
-				
-				return myList;
-			}
-			
-			
-			
+	ObjectProperty<ObservableList<String>> listviewProp = new SimpleObjectProperty<ObservableList<String>>();
+	public ObjectProperty<ObservableList<String>> listviewProperty() throws IOException {
+		listviewProp.setValue(FXCollections.observableList(getList()));
+		return listviewProp;
+	}
 	
+	public void setListviewProp(ObservableList<String> newListviewProp){
+		listviewProp.setValue(newListviewProp);	
+	}
+	
+	public ObservableList<String> getListviewProp(){
+		return listviewProp.getValue();
+	}
+	
+	public List<String> myList = new ArrayList<String>();
+	public List<String> getList() throws IOException{
+		myList = WebCrawler.crawl();
+		
+		return myList;
+	}
+			
 }
