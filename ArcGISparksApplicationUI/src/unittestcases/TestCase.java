7cc66f6bfc20471ebda461a536428f1b891e74fb
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-03-05 21:55:29 -0500

    updated test case

--- ArcGISparksApplicationUI/src/unittestcases/TestCase.java
+++ ArcGISparksApplicationUI/src/unittestcases/TestCase.java
@@ -11,6 +11,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -56,5 +57,121 @@ public class TestCase {
 		return imageURL;
 	}
 	
+	@Test
+	public void getRecAreaIdsTest() throws IOException {
+		
+		for(int i = 2500; i < 3500; i++){
+			String testURL0 = "https://www.recreation.gov/recreationalAreaDetails.do?contractCode=NRSO&recAreaId="+i+"&agencyCode=70904";
+			String name = getRecAreaIds(testURL0);
+			if(name.length()> 5 && name.endsWith("1") && name.contains("National Park")) {
+				System.out.println(i+" " +name.substring(0, name.length()-2));
+				//System.out.println("          "+getCampGroundList(testURL0));
+			}
+		}
+		
+	}
+	
+	@SuppressWarnings("finally")
+	private String getRecAreaIds(String url) throws IOException {
+		String imageURL = "";
+		//
+		URL website = new URL(url);
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		FileOutputStream fos = new FileOutputStream("information.txt");
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		
+		BufferedReader br = new BufferedReader(new FileReader("information.txt"));
+		try {
+		    StringBuilder sb = new StringBuilder();
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        line = br.readLine();
+		        if(line.matches("^.*<title>Recreation.gov recreation area details.*$")) {
+		        	int indexStart = line.indexOf("-");
+		        	int indexEnd = line.lastIndexOf("-");
+		        	imageURL = line.substring(indexStart, indexEnd-1);
+		        }
+		        if(line.matches("^.*<span class='locationsubtitle'>National Park Service</span></div>.*$")) {
+		        	imageURL = imageURL+" 1";
+		        }
+		        //<span class='locationsubtitle'>National Park Service</span></div>
+		    }
+		} finally {
+		    br.close();
+		    return imageURL;
+		}
+		//
+		//return imageURL;
+	}
+	
+	@Test
+	public void campGroundListTest() throws IOException {
+		
+		
+		
+		String testURL1 = "https://www.recreation.gov/recreationalAreaDetails.do?contractCode=NRSO&recAreaId=2933&agencyCode=70904";
+		ArrayList<String> myList1 = new ArrayList<String>();
+		myList1.add("BLACKWOODS CAMPGROUND");
+		myList1.add("DUCK HARBOR CAMPGROUND");
+		myList1.add("Schoodic Woods Campground");
+		myList1.add("SEAWALL CAMPGROUND");		
+		assertEquals(myList1,
+				getCampGroundList(testURL1));
+	}	
+	
+	public enum ParserState {
+	    NONE, CAMPGROUND 
+	}
+	
+	@SuppressWarnings("finally")
+	private ArrayList<String> getCampGroundList(String url) throws IOException {
+		ParserState parserState = ParserState.NONE;
+		//
+		ArrayList<String> dataList = new ArrayList<String>();
+		//
+		URL website = new URL(url);
+		ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		FileOutputStream fos = new FileOutputStream("information.txt");
+		fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+		
+		BufferedReader br = new BufferedReader(new FileReader("information.txt"));
+		try {
+		    StringBuilder sb = new StringBuilder();
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        line = br.readLine();
+		        if(parserState == ParserState.NONE) {
+		        	if(line.matches("^.*MAKE A RESERVATION.*$")) {
+			        	parserState = ParserState.CAMPGROUND;
+			        }
+		        }
+		        if(parserState == ParserState.CAMPGROUND) {
+		        	if(line.matches("^.*\\d'    >.*$")) {
+		        		
+		        		int index = 0;
+		        		int indexStart = 0;
+		        		int indexEnd = 0;
+		        		while(indexStart <= index) {
+		        			indexStart = line.substring(index).indexOf("    >")+index;
+			        		indexEnd = line.substring(indexStart).indexOf("&")+indexStart;
+			        		dataList.add(line.substring(indexStart+5, indexEnd));
+			        		
+			        		index = indexEnd;
+		        		}
+		        		
+			        	parserState = ParserState.NONE;
+		        	}
+		        }
+		        
+		        
+		        
+		    }
+		} finally {
+		    br.close();
+		    return dataList;
+		}
+	}
 
 }
