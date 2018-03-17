cbb13f759c96354859b564d53c7cc3fc8b038df3
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2018-03-17 15:22:10 -0400

    minor fix

--- ArcGISparksApplicationUI/src/controllers/ParkController.java
+++ ArcGISparksApplicationUI/src/controllers/ParkController.java
@@ -1,6 +1,5 @@
 package controllers;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -28,7 +27,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.Accordion;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
